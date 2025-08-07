b255530edef5a2063f7ec42f2dd78ae243bf35e8
Talles Souza <tallessouza117@gmail.com>
2025-08-07 17:11:37 -0300

    Exceções 3

---
+++ src/maratonajava/javacore/Oexception/exception/dominio/Funcionario.java
@@ -0,0 +1,9 @@
+package maratonajava.javacore.Oexception.exception.dominio;
+
+import java.io.FileNotFoundException;
+
+public class Funcionario extends Pessoa{
+    public void salvar() throws LoginInvalidoException, ArithmeticException {
+        System.out.println("Salvando funcionario");
+    }
+}
