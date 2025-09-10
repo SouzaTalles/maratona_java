938596612fe3a10958095be26944686197aa3ea8
Talles Souza <tallessouza117@gmail.com>
2025-09-10 12:01:38 -0300

    ResourceBundle

---
+++ src/maratonajava/javacore/Tresourcebundle/test/ResourceBundleTest01.java
@@ -0,0 +1,26 @@
+package maratonajava.javacore.Tresourcebundle.test;
+
+import maratonajava.javacore.Gassociacao.exercicio.dominio.Local;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class ResourceBundleTest01 {
+    public static void main(String[] args) {
+        System.out.println(Locale.getDefault());
+        ResourceBundle bundle = ResourceBundle.getBundle("message", Locale.of("en", "US"));
+        System.out.println(bundle.getString("hello"));
+        System.out.println(bundle.getString("good.morning"));
+        System.out.println(bundle.getString("hi"));
+        bundle = ResourceBundle.getBundle("message", Locale.of("pt", "BR"));
+        System.out.println(bundle.getString("hello"));
+        System.out.println(bundle.getString("good.morning"));
+        // Locale("fr", "CA")
+        //message_fr_CA.properties
+        //message_fr.properties
+        //message_pt_BR.properties
+        //message_pt.properties
+        //message.properties
+        System.out.println(bundle.getString("hi"));
+    }
+}
