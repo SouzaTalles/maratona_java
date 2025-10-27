6c256af5f088d1210572cd5a7c262c547213f4f9
Talles Souza <tallessouza117@gmail.com>
2025-10-27 15:19:32 -0300

    Itarator e Set, HashSet

--- src/maratonajava/javacore/Ycolecoes/dominio/Manga.java
+++ src/maratonajava/javacore/Ycolecoes/dominio/Manga.java
@@ -6,6 +6,7 @@ public class Manga implements Comparable<Manga> {
     private Long id;
     private String nome;
     private double preco;
+    private int quantidade;
 
     public Manga(Long id, String nome, double preco) {
         Objects.requireNonNull(id, "Id não pode ser null");
@@ -15,24 +16,30 @@ public class Manga implements Comparable<Manga> {
         this.preco = preco;
     }
 
+    public Manga(Long id, String nome, double preco, int quantidade) {
+        this(id, nome, preco);
+        this.quantidade = quantidade;
+    }
+
     @Override
     public String toString() {
         return "Manga{" +
                 "id=" + id +
                 ", nome='" + nome + '\'' +
                 ", preco=" + preco +
+                ", quantidade=" + quantidade +
                 '}';
     }
 
     @Override
     public boolean equals(Object o) {
         if (!(o instanceof Manga manga)) return false;
-        return Double.compare(preco, manga.preco) == 0 && Objects.equals(id, manga.id) && Objects.equals(nome, manga.nome);
+        return Objects.equals(id, manga.id) && Objects.equals(nome, manga.nome);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, nome, preco);
+        return Objects.hash(id, nome);
     }
 
     @Override
@@ -78,5 +85,13 @@ public class Manga implements Comparable<Manga> {
 
     public void setPreco(double preco) {
         this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
     }
 }
---
+++ src/maratonajava/javacore/Ycolecoes/test/SetTest01.java
@@ -0,0 +1,22 @@
+package maratonajava.javacore.Ycolecoes.test;
+
+import maratonajava.javacore.Ycolecoes.dominio.Manga;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetTest01 {
+    public static void main(String[] args) {
+        Set<Manga> mangas = new HashSet<>();
+        mangas.add(new Manga(5L,"Berserk", 19.9, 0));
+        mangas.add(new Manga(1L,"Hellsing Ultimate", 9.5, 5));
+        mangas.add(new Manga(4L,"Pokemon", 3.2,0));
+        mangas.add(new Manga(3L,"Attack on Titan", 11.2, 2));
+        mangas.add(new Manga(2L,"Dragon ball Z", 2.99, 0));
+
+        for (Manga manga : mangas) {
+            System.out.println(manga);
+        }
+
+    }
+}
