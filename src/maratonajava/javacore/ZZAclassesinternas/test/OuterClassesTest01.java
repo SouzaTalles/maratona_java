ca347cfd689d9517db7c26b4612fe4977ae3f2f8
Talles Souza <tallessouza117@gmail.com>
2025-11-01 12:04:41 -0300

    Classes internas

---
+++ src/maratonajava/javacore/ZZAclassesinternas/test/OuterClassesTest01.java
@@ -0,0 +1,22 @@
+package maratonajava.javacore.ZZAclassesinternas.test;
+
+public class OuterClassesTest01 {
+    private String name = "Monkei D. Luffy";
+
+    class Inner {
+        public void printOuterClassAtrribute() {
+            System.out.println("Name: " + name);
+            System.out.println(this);
+            System.out.println(OuterClassesTest01.this);
+        }
+    }
+
+    public static void main(String[] args) {
+        OuterClassesTest01 outerClass = new OuterClassesTest01();
+        Inner inner = outerClass.new Inner();
+        inner.printOuterClassAtrribute();
+
+        Inner inner2 = new OuterClassesTest01().new Inner();
+        inner2.printOuterClassAtrribute();
+    }
+}
