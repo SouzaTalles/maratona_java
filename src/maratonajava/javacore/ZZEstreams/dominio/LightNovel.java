096c0a9dc1ffc36017d33a58d28141402fabbadd
Talles Souza <tallessouza117@gmail.com>
2025-11-11 12:07:59 -0300

    Streams

---
+++ src/maratonajava/javacore/ZZEstreams/dominio/LightNovel.java
@@ -0,0 +1,40 @@
+package maratonajava.javacore.ZZEstreams.dominio;
+
+import java.util.Objects;
+
+public class LightNovel {
+    private String title;
+    private double price;
+
+    public LightNovel(String title, double price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "LightNovel{" +
+                "title='" + title + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof LightNovel that)) return false;
+        return Objects.equals(title, that.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(title);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
