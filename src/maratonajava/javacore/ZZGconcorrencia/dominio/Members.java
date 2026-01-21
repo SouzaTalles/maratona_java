422d9f0927124caf6e94d775ff7ed4343d354a56
Talles Souza <tallessouza117@gmail.com>
2026-01-20 21:19:09 -0500

    Concorrência

--- src/maratonajava/javacore/ZZGconcorrencia/dominio/Members.java
+++ src/maratonajava/javacore/ZZGconcorrencia/dominio/Members.java
@@ -1,10 +1,15 @@
-package maratonajava.javacore.ZZFthhreads.dominio;
+package maratonajava.javacore.ZZGconcorrencia.dominio;
 
+import java.util.PrimitiveIterator;
 import java.util.Queue;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Members {
-    private  final Queue<String> emails = new ArrayBlockingQueue<>(10);
+    private final Queue<String> emails = new ArrayBlockingQueue<>(10);
+    private final ReentrantLock lock = new ReentrantLock();
+    private final Condition condition = lock.newCondition();
     private boolean open = true;
 
     public boolean isOpen() {
@@ -12,41 +17,53 @@ public class Members {
     }
 
     public int pendingEmails() {
-        synchronized (emails) {
+        lock.lock();
+        try {
             return emails.size();
+        } finally {
+            lock.unlock();
         }
     }
 
     public void addMenberEmail(String email) {
-        synchronized (this.emails) {
+        lock.lock();
+        try {
             String threadName = Thread.currentThread().getName();
             System.out.println(threadName + " Adicionou email na lista");
             this.emails.add(email);
-            this.emails.notifyAll();
+            condition.signalAll();
+        } finally {
+            lock.unlock();
         }
     }
 
-    public String retriveEmail()  {
+    public String retriveEmail() {
         System.out.println(Thread.currentThread().getName() + " checking if are emails");
-        synchronized (this.emails) {
+        lock.lock();
+        try {
             while (emails.isEmpty()) {
-                if(!open) return null;
+                if (!open) return null;
                 System.out.println(Thread.currentThread().getName() + " Não tem emails disponível na lista, entrando em modo de espera");
                 try {
-                    this.emails.wait();
+                    condition.await();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
             return this.emails.poll();
+        } finally {
+            lock.unlock();
         }
     }
 
     public void close() {
         open = false;
-        synchronized (this.emails) {
+        lock.lock();
+        try {
             System.out.println(Thread.currentThread().getName() + " Notificando todo mundo que não estamos mais pegando emails");
-            this.emails.notifyAll();
+            condition.signalAll();
+        } finally {
+            lock.unlock();
         }
     }
 }
---
+++ src/maratonajava/javacore/ZZGconcorrencia/test/EmailDeliveryTest01.java
@@ -0,0 +1,25 @@
+package maratonajava.javacore.ZZGconcorrencia.test;
+
+
+import maratonajava.javacore.ZZGconcorrencia.dominio.Members;
+import maratonajava.javacore.ZZGconcorrencia.service.EmailDeliveryService;
+
+import javax.swing.*;
+
+public class EmailDeliveryTest01 {
+    public static void main(String[] args) {
+        Members members = new Members();
+        Thread jiraya = new Thread(new EmailDeliveryService(members), "Jiraya");
+        Thread kakashi = new Thread(new EmailDeliveryService(members), "Kakashi");
+        jiraya.start();
+        kakashi.start();
+        while (true) {
+            String email = JOptionPane.showInputDialog("Entre com seu email");
+            if(email == null || email.isEmpty()) {
+                members.close();
+                break;
+            }
+            members.addMenberEmail(email);
+        }
+    }
+}
