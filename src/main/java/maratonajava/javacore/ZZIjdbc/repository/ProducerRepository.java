e55460b43413f5a00df2073e4492345e2d083673
Talles Souza <tallessouza117@gmail.com>
2026-02-12 14:44:26 -0300

    findAll

--- src/main/java/maratonajava/javacore/ZZIjdbc/repository/ProducerRepository.java
+++ src/main/java/maratonajava/javacore/ZZIjdbc/repository/ProducerRepository.java
@@ -5,12 +5,15 @@ import maratonajava.javacore.ZZIjdbc.conn.ConnectionFactory;
 import maratonajava.javacore.ZZIjdbc.dominio.Producer;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 @Log4j2
 public class ProducerRepository {
-    public static void save(Producer producer){
+    public static void save(Producer producer) {
         String sql = "INSERT INTO `anime_store`.`producer` (`name`) VALUES ('%s');".formatted(producer.getName());
         try (Connection conn = ConnectionFactory.getConnection();
              Statement stmt = conn.createStatement()) {
@@ -21,7 +24,7 @@ public class ProducerRepository {
         }
     }
 
-    public static void delete(int id){
+    public static void delete(int id) {
         String sql = "DELETE FROM `anime_store`.`producer` WHERE (`id` = '%d');".formatted(id);
         try (Connection conn = ConnectionFactory.getConnection();
              Statement stmt = conn.createStatement()) {
@@ -32,7 +35,7 @@ public class ProducerRepository {
         }
     }
 
-    public static void update(Producer producer){
+    public static void update(Producer producer) {
         String sql = "UPDATE `anime_store`.`producer` SET `name` = '%s' WHERE (`id` = '%d');"
                 .formatted(producer.getName(), producer.getId());
         try (Connection conn = ConnectionFactory.getConnection();
@@ -42,5 +45,29 @@ public class ProducerRepository {
         } catch (SQLException e) {
             log.error("Error while trying to update producer '{}'", producer.getId(), e);
         }
+    }
+
+    public static List<Producer> findAll() {
+        log.info("Finding all producers");
+        String sql = "SELECT id, name FROM anime_store.producer;";
+        List<Producer> producers = new ArrayList<>();
+
+        try (Connection conn = ConnectionFactory.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                Producer producer = Producer
+                        .builder()
+                        .id(rs.getInt("id"))
+                        .name(rs.getString("name"))
+                        .build();
+                producers.add(producer);
+            }
+
+        } catch (SQLException e) {
+            log.error("Error while trying to find all producers", e);
+        }
+        return producers;
     }
 }
--- src/main/java/maratonajava/javacore/ZZIjdbc/service/ProducerService.java
+++ src/main/java/maratonajava/javacore/ZZIjdbc/service/ProducerService.java
@@ -3,6 +3,8 @@ package maratonajava.javacore.ZZIjdbc.service;
 import maratonajava.javacore.ZZIjdbc.dominio.Producer;
 import maratonajava.javacore.ZZIjdbc.repository.ProducerRepository;
 
+import java.util.List;
+
 public class ProducerService {
 
     public static void seve(Producer producer) {
@@ -17,6 +19,10 @@ public class ProducerService {
     public static void update(Producer producer) {
         requireValidId(producer.getId());
         ProducerRepository.update(producer);
+    }
+
+    public static List<Producer> findAll() {
+        return ProducerRepository.findAll();
     }
 
     private static void requireValidId(Integer id) {
